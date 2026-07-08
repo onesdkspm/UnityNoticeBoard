4772f88115b0becaae7b205dcb1b9e4ecc2b1a14
m2构造机 <zhanggaoqi@aobi.com>
2026-07-08 18:01:16 +0800

    [Branch Release] v2.0.3-dev-1481473 - develop
    - 更新所有 XCFrameworks 到 2.0.3-dev-1481473
    - 更新资源文件
    - 更新 checksums
    - 分支测试版本（无 tag）

--- Package.swift
+++ Package.swift
@@ -11,10 +11,10 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/onesdkspm/AOUnityWebBridge.git", from: "2.0.2"),
-        .package(url: "https://github.com/onesdkspm/BTSimpleHTTPNetworking.git", from: "2.0.2"),
-        .package(url: "https://github.com/onesdkspm/UnityBridge.git", from: "2.0.0"),
-        .package(url: "https://github.com/onesdkspm/BTSDKUIKitCore.git", from: "1.0.0"),
+        .package(url: "https://github.com/onesdkspm/AOUnityWebBridge.git", from: "2.0.3"),
+        .package(url: "https://github.com/onesdkspm/BTSimpleHTTPNetworking.git", from: "2.0.3"),
+        .package(url: "https://github.com/onesdkspm/UnityBridge.git", from: "2.0.3"),
+        .package(url: "https://github.com/onesdkspm/BTSDKUIKitCore.git", from: "2.0.3"),
     ],
     targets: [
         // ========== Wrapper Target（统一管理系统依赖）==========
@@ -38,8 +38,8 @@ let package = Package(
         // ========== Binary Frameworks ==========
         .binaryTarget(
             name: "UnityNoticeBoard",
-            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/UnityNoticeBoard/2.0.2-dev-1480317/UnityNoticeBoard.xcframework.zip",
-            checksum: "ad290cadba96a732892dcccfac309c8a73594bf0d96af9fbfe30d954ff81affc"
+            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/UnityNoticeBoard/2.0.3-dev-1481473/UnityNoticeBoard.xcframework.zip",
+            checksum: "13b89c2d048654c52d8b67de32b1c555680cb08eac372596a94617f64300befc"
         )
     ]
 )
